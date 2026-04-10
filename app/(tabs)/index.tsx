b8758e361acb3fcf0a08a0ebd0d9f8ae7cd1c9d5
yeswadams <yeswaadams73@gmail.com>
2026-04-10 14:37:43 +0300

    This branch is for making updates on the home page before merging

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
 import "@/global.css";
-import { Text, View } from "react-native";
+import { Text} from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import { styled } from "nativewind";
+
+const SafeAreaView = styled(RNSafeAreaView)
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 items-center justify-center bg-background">
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
@@ -30,6 +34,6 @@ export default function App() {
       }}>
         Subscriptions
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
